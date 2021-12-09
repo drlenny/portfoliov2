14c1dade2a4b32a0ffc98f2e35cec722a76fb8a3
drlenny <66442932+drlenny@users.noreply.github.com>
2021-12-09 15:01:01 -0600

    install mui components and add navbar and image folder

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
+import MyNavbar from "./Navbar";
 
 function App() {
   return (
     <div>
+      <MyNavbar />
       <Header />
       <h2>Sup</h2>
       <Footer />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,21 @@
 import React from "react";
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
 
 function Header() {
     return (
-        <h1>Hello</h1>
+        <Container>
+        <Box sx={{ my: 2 }}>
+          {[...new Array(12)]
+            .map(
+              () => `Cras mattis consectetur purus sit amet fermentum.
+Cras justo odio, dapibus ac facilisis in, egestas eget quam.
+Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`,
+            )
+            .join('\n')}
+        </Box>
+      </Container>
     );
 }
 
