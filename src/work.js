125cb0ea6549b952a345f62341a9112aa8fbd09f
drlenny <66442932+drlenny@users.noreply.github.com>
2021-12-24 22:34:49 -0600

    adding more to work array

--- src/work.js
+++ src/work.js
@@ -2,7 +2,11 @@ const work = [
     {
         key: 1,
         title: "This Website",
-        content: "You're looking at it!"
+        content: "You're looking at it!",
+        image: "",
+        alt: "",
+        link: "",
+        code: "https://github.com/drlenny/portfoliov2"
     },
     {
         key: 2,
@@ -10,31 +14,44 @@ const work = [
         content: "This is an app that allows users to create and categorize any upcoming tasks that need to be completed. Seperate lists can be made and within each list the user can add and delete items that pertain to the list's topic. An express backend is used to store the input information in a Postgres database.",
         image: 0,
         alt: "List-It App",
-        link: "https://my-todo-list-project.herokuapp.com/"
+        link: "https://my-todo-list-project.herokuapp.com/",
+        code: "https://github.com/drlenny/todo-list"
     },
     {
         key: 3,
         title: "Keeper",
         content: "Keeper is a notetaking app with a simple minimalist design and multiple responsive elements. Allowing the user to manipulate each note taking area seperately allows for more ease of use and better organization. This was built built with React using JSX. An express backend with a MongoDB database is in the process of being implemented.",
         image: 1,
-        alt: "Keeper App"
+        alt: "Keeper App",
+        link: "https://my-react-keeper-app.herokuapp.com/",
+        code: "https://github.com/drlenny/keeper-app"
     },
     {
         key: 3,
         title: "Daily Journal",
         content: "It's a blog",
+        image: "",
+        alt: "",
+        link: "https://journaldailyblog.herokuapp.com/",
+        code: "https://github.com/drlenny/daily-journal-blog"
     },
     {
         key: 4,
         title: "Secrets",
-        content: "Ever had a secret nagging at your mind and no way to express it? Well this application helps you doi just that! This app was designed to be a safe place to anonymously share their thoughts and ideas as well as view what other secrets other unknown people may have. This app was an exercise using express and mongoose to develop a secure authentication design with a database populated by private users and their passwords. I'm quite pleased with the end result and I definitely learned a lot about designing authentication security which I believe is critically important for users seeking privacy in a world so dependant on a publically online presence."
+        content: "Ever had a secret nagging at your mind and no way to express it? Well this application helps you doi just that! This app was designed to be a safe place to anonymously share their thoughts and ideas as well as view what other secrets other unknown people may have. This app was an exercise using express and mongoose to develop a secure authentication design with a database populated by private users and their passwords. I'm quite pleased with the end result and I definitely learned a lot about designing authentication security which I believe is critically important for users seeking privacy in a world so dependant on a publically online presence.",
+        image: "",
+        alt: "",
+        link: "https://user-secret.herokuapp.com/",
+        code: "https://github.com/drlenny/Authentication-Secrets"
     },
     {
         key: 5,
         title: "Simon Says",
         content: "This was an early project of mine done to practice advanced CSS and jQuery selectors. It's a fully interactive web browser game based on Simon Says. After initiating a new game, a panel is highlighted and awaits the user input and conitnues if it matches the generated pattern. The game memorizes the pattern and keeps track of the number of successful rounds until the user input is incorrect resulting in a game over.",
         image: 2,
-        alt: "Simon Says App"
+        alt: "Simon Says App",
+        link: "https://simon-says-app.herokuapp.com/",
+        code: "https://github.com/drlenny/simon-game"
     },
 ];
 
