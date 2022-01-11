117689f99172ac087c9b15633ec4b57eebe882df
drlenny <66442932+drlenny@users.noreply.github.com>
2022-01-10 20:33:33 -0600

    condensing code for nav links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
@@ -8,9 +8,11 @@ import Box from '@mui/material/Box';
 import Fab from '@mui/material/Fab';
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 import Zoom from '@mui/material/Zoom';
+import MenuIcon from '@mui/icons-material/Menu';
 
 import Resume from '../media/lenihanResume.pdf';
 
+// may end up not needing or wanting this scroll part
 function ScrollTop(props) {
 
   const { children, window } = props;
@@ -46,40 +48,71 @@ function ScrollTop(props) {
   );
 }
 
-
+const pages = ['portfolio', 'contact', 'resume']
 
 function Navbar(props) {
+
+  const [anchorElNav, setAnchorElNav] = useState(null);
+
+  const handleOpenMenu = (event) => {
+    setAnchorElNav(event.currentTarget)
+  };
+
+  const handleCloseMenu = (event) => {
+    setAnchorElNav(null)
+  };
+
+  // setting attributes specifically to the resume link
+  useEffect(() => {
+    document.getElementById('resume-link').setAttribute('target', '_blank');
+    document.getElementById('resume-link').setAttribute('href', Resume)
+  }, [])
 
   return (
     <React.Fragment>
       <CssBaseline />
-      
+
       <AppBar>
         <Toolbar>
-          <Typography variant="h4" component="div" sx={{ flexGrow: 1 }}>
+
+          {/* logo */}
+          <Typography variant="h4" noWrap component="div" sx={{ flexGrow: 1, fontVariant: 'small-caps' }}>
             Daniel Lenihan
           </Typography>
 
-          <a href='#portfolio' className='nav-link'>
-            <Typography variant="h6" component="div" sx={{ mr: 4 }}>
-              Portfolio
-            </Typography>
-          </a>
+          {/* navigation */}
+          {/* <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+            <a href='#portfolio' className='nav-link'>
+              <Typography variant="h6" component="div" sx={{ mr: 4, fontVariant: 'small-caps' }}>
+                Portfolio
+              </Typography>
+            </a>
+            <a href={Resume} target='_blank' className='nav-link'>
+              <Typography variant="h6" component="div" sx={{ mr: 4 }}>
+                Resume
+              </Typography>
+            </a>
+            <a href='#contact' className='nav-link'>
+              <Typography variant="h6" component="div" sx={{ mr: 4 }}>
+                Contact
+              </Typography>
+            </a>
+          </Box> */}
 
-          <a href={Resume} target='_blank' className='nav-link'>
-            <Typography variant="h6" component="div" sx={{ mr: 4 }}>
-              Resume
-            </Typography>
-          </a>
-
-          <a href='#contact' className='nav-link'>
-            <Typography variant="h6" component="div" sx={{ mr: 4 }}>
-              Contact
-            </Typography>
-          </a>
+          {/* navigation */}
+          <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+            {pages.map((page) => (
+              <a href={`#${page}`} className='nav-link' id={`${page}-link`}>
+                <Typography key={page} variant="h6" component="div" sx={{ mr: 4, fontVariant: 'small-caps' }}>
+                  {`${page.charAt(0).toUpperCase() + page.slice(1)}`}
+                </Typography>
+              </a>
+            ))}
+          </Box>
 
         </Toolbar>
       </AppBar>
+
       <Toolbar id="back-to-top-anchor" />
       <ScrollTop {...props}>
         <Fab color="secondary" size="small" aria-label="scroll back to top">
